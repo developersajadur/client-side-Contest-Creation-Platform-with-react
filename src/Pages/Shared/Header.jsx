dc4e0fb31e389f53383ff77c766ec91cd5c7752e
itzmesojib <itzmesojib@gmail.com>
2024-06-01 16:42:50 +0600

    react, tailwind, and ui install and basic header set up

---
+++ src/Pages/Shared/Header.jsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet";
+  
+
+const Header = () => {
+    const navLinks = (
+        <>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/">Contact</NavLink></li>
+            <li><NavLink to="/">Shop</NavLink></li>
+        </>
+    );
+    
+    return (
+        <div className="w-full flex justify-between h-20 items-center bg-slate-500 px-1 lg:px-10">
+            <div className="block lg:hidden">
+                <Sheet>
+                    <SheetTrigger><GiHamburgerMenu className="text-2xl" /></SheetTrigger>
+                    <SheetContent side="left" className="bg-black text-white">
+                        <SheetHeader>
+                            <SheetTitle className="text-3xl">Contest</SheetTitle>
+                        </SheetHeader>
+                        <ul className="space-y-4">{navLinks}</ul>
+                    </SheetContent>
+                </Sheet>
+            </div>
+            <h1 className="text-3xl font-bold">Contest</h1>
+            <div className="hidden lg:block">
+                <ul className="flex gap-8 font-semibold sm:list-none text-lg">
+                    {navLinks}
+                </ul>
+            </div>
+            <Button className="bg-orange-500 hover:bg-red-500 rounded-[5px]" variant="destructive">Login</Button>
+        </div>
+    );
+};
+
+export default Header;
